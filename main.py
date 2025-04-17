400df799b8597262f358b40094a53a83df5262e9
romance <106858958+romanceOSX@users.noreply.github.com>
2025-04-16 22:02:39 -0600

    chore: did some inspection on construction

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class MyClass:
 # --> https://docs.python.org/3/library/ipaddress.html
 def _test_ipaddress() -> None:
     # we can create addresses from a generic factory function
+    # note that a standalone address does not describe a network
     ip4_addr = ip.ip_address("127.0.0.1")
     ip6_addr = ip.ip_address("fe80::1")
     # there is no __bytes__ magic method, use packed instead
@@ -28,11 +29,28 @@ def _test_ipaddress() -> None:
     print(f"Directly from ipv4 class: {ip4_specific_addr=}")
 
     # we can also create networks
+    # note that a network does not describe specific addresses
     my_network = ip.ip_network("172.16.0.0/24")
+    print(f"{my_network.with_netmask=}")
+    print(f"{my_network.broadcast_address}")
     #my_network = ip.ip_network("172.16.0.17/24") # <-- fails, note that we should not declare host bits
     network_hosts = my_network.hosts()
     #for host in network_hosts:
     #    print(host)
+
+    # an interface encapsulates both an specific address that lives inside a network
+    
+    #my_interface = ip.ip_interface("172.16.0.3/24")
+    my_interface = ip.ip_interface(ip4_addr)
+    if isinstance(ip4_addr, (bytes, int)):
+        print("It is instance!")
+    print(f"{my_interface=}")
+
+# how does it get constructed internally?
+# you can pass different objects to initialize the ip.Interface class
+# these constructors get evaluated through calling @classmethods at the interface.__init__
+# in this way we can evaluate from where we are constructor
+# effectively immitating the overload rulesof c++
 
 def _test_formatting() -> None:
     a = 32
